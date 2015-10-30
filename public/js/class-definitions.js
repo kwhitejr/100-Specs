da0249f277a0913b2f05cbbe973b51728ea8b455
kwhitejr <kwhitejr.edu@gmail.com>
2015-10-29 17:33:13 -1000

    Step 19 Beers Passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -228,7 +228,7 @@ var laptopCosts = {
  * "Species"
  *
  */
-
+var livingOrganismClassification = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"];
 
 /* Step 18
  *
@@ -242,7 +242,7 @@ var laptopCosts = {
  * "cvs"
  *
  */
-
+var scmList = ["git", "svn", "mercurial", "bazaar", "cvs"];
 
 /* Step 19
  *
@@ -261,7 +261,14 @@ var laptopCosts = {
  *                  "Golden"
  *
  */
-
+var beers = {
+	IPA: "Ale",
+	Lager: "Strong",
+	Heffeweisen: "German",
+	Stout: ["Thick", "Dark"],
+	Porter: "Bitter",
+	Ale: ["Light", "Golden"]
+};
 
 /* Step 20
  *
